0d8df0890078e9668db9d70759e2a3631406697b
y-nagai0725 <y.nagai0725@gmail.com>
2025-10-16 21:37:28 +0900

    home画面、日記表示部分作成中

--- app-front/src/views/HomeView.vue
+++ app-front/src/views/HomeView.vue
@@ -10,7 +10,7 @@ import { userName } from '@/auth.js';
 
 const notice = ref('');
 const recentDiaries = ref([]);
-const totalDiaries = ref(null);
+const totalDiariesCount = ref(null);
 
 /**
  * 日記未登録時のメッセージ
@@ -62,7 +62,7 @@ onMounted(async () => {
   //直近5件の日記データ取得、表示
   const responseData = await getRecentDiaries();
   recentDiaries.value = responseData.recentDiaries;
-  totalDiaries.value = responseData.totalDiaries;
+  totalDiariesCount.value = responseData.totalDiaries;
 
   //お知らせメッセージ表示
   if (recentDiaries.value.length === 0 || !existsTodayDiaryData()) {
@@ -79,7 +79,7 @@ onMounted(async () => {
           <div class="home__information-grid-header">登録名：</div>
           <div class="home__information-grid-item">{{ userName }}</div>
           <div class="home__information-grid-header">投稿数：</div>
-          <div class="home__information-grid-item">{{ totalDiaries }}</div>
+          <div class="home__information-grid-item">{{ totalDiariesCount }}</div>
         </div>
         <div class="home__information-link-wrapper">
           <RouterLink class="home__link-create" to="/diary/new"
@@ -97,36 +97,35 @@ onMounted(async () => {
       <p v-if="recentDiaries.length === 0" class="home__no-diaries">
         {{ NO_DIARIES_MESSAGE }}
       </p>
-      <div v-else class="home__diaries-grid">
-        <div class="home__grid-header">日付</div>
-        <div class="home__grid-header">日記内容</div>
-        <div class="home__grid-header">Geminiコメント有無</div>
-        <div class="home__grid-header">編集リンク</div>
-        <div
+      <ul v-else class="home__diaries-list">
+        <li
           v-for="diary in recentDiaries"
           :key="diary.id"
-          class="home__grid-row"
+          class="home__diary-item"
         >
-          <p class="home__grid-item">{{ formatDate(diary.date) }}</p>
-          <p class="home__grid-item">{{ diary.text }}</p>
-          <p class="home__grid-item home__gemini-check">
-            <CheckmarkIcon v-if="diary.geminiComment" />
-          </p>
-          <p class="home__grid-item">
-            <RouterLink class="home__edit-link" :to="`/diary/${diary.id}`"
-              >編集</RouterLink
+          <RouterLink class="home__diary-edit-link" :to="`/diary/${diary.id}`">
+            <div class="home__diary-date-wrapper">
+              <span class="home__diary-date">11</span>
+              <span class="home__diary-day">水</span>
+              <span class="home__diary-time">11:35</span>
+              <span class="home__diary-yyyy-mm">2025/10</span>
+            </div>
+            <div
+              class="home__diary-text-wrapper"
+              :class="{ 'exists-gemini-comment': diary.geminiComment }"
             >
-          </p>
-        </div>
-      </div>
+              <p class="home__diary-text">{{ diary.text }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
 //TODOメモ css(デザイン)はほとんど全部まだ仮です、最後に作成します。
 .home {
-  display: flex;
-  flex-direction: column;
+  display: grid;
   gap: 1.6rem;
 
   @include tab {
@@ -134,7 +133,8 @@ onMounted(async () => {
   }
 
   @include pc {
-    flex-direction: row;
+    align-items: start;
+    grid-template-columns: minmax(300px, 40rem) auto;
     gap: 10rem;
   }
 
@@ -150,7 +150,7 @@ onMounted(async () => {
     }
 
     @include pc {
-      width: calc(100% / 3);
+      width: 100%;
       margin-inline: initial;
       gap: 3.2rem;
     }
@@ -283,36 +283,131 @@ onMounted(async () => {
     }
   }
 
-  &__diaries-grid {
+  &__recent-diaries {
     display: grid;
-    grid-template-columns: 2fr 3fr 1fr 1fr;
-  }
+    gap: 1rem;
 
-  &__grid-header {
-  }
+    @include tab {
+      gap: 1.6rem;
+    }
 
-  &__grid-row {
-    display: contents;
-  }
-
-  &__grid-item {
-    &:nth-of-type(2) {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
+    @include pc {
+      gap: 2.4rem;
     }
   }
 
-  &__gemini-check {
+  &__sub-title {
+    text-align: center;
+    font-weight: 700;
+    font-size: clamp(22px, 2.2rem, 26px);
+    letter-spacing: 0.1em;
+
+    @include tab {
+      font-size: clamp(26px, 2.6rem, 30px);
+    }
+
+    @include pc {
+      font-size: clamp(30px, 4rem, 40px);
+    }
+  }
+
+  &__no-diaries {
+    padding: 1em;
+    background-color: $white;
+    border-radius: 10px;
+    color: $red;
+    font-size: clamp(14px, 1.4rem, 15px);
+    line-height: 1.8;
+
+    @include tab {
+      font-size: clamp(15px, 1.5rem, 16px);
+    }
+
+    @include pc {
+      font-size: clamp(14px, 1.6rem, 16px);
+    }
+  }
+
+  &__diaries-list {
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
+    gap: 2.4rem;
 
-    svg {
-      width: 20px;
-      height: 20px;
-      color: #42b983;
+    @include tab {
+      gap: 3.2rem;
     }
+
+    @include pc {
+      gap: 4rem;
+    }
+  }
+
+  &__diary-item {
+  }
+
+  &__diary-edit-link {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__diary-date-wrapper {
+    display: flex;
+  }
+
+  &__diary-date {
+  }
+
+  &__diary-day {
+  }
+
+  &__diary-time {
+  }
+
+  &__diary-yyyy-mm {
+  }
+
+  &__diary-text-wrapper {
+    background-color: $white;
+    border-radius: 10px;
+    padding: 1em;
+    font-size: clamp(16px, 1.6rem, 17px);
+    position: relative;
+
+    &::after {
+      content: 'Geminiコメント無し';
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translateY(-50%);
+      background-color: $red;
+      color: $white-brown;
+      padding: 0.5em 0.75em;
+      border-radius: 10px;
+      font-size: 0.75em;
+    }
+
+    @include tab {
+      font-size: clamp(17px, 1.7rem, 18px);
+    }
+
+    @include pc {
+      font-size: clamp(16px, 1.8rem, 18px);
+    }
+
+    &.exists-gemini-comment {
+      &::after {
+        content: 'Geminiコメント有り';
+        background-color: $green;
+      }
+    }
+  }
+
+  &__diary-text {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
+    text-overflow: ellipsis;
   }
 }
 </style>
